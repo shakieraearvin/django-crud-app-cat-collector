e4c9f168cdf4555fe54543c6f8c5dc9a4eada38c
Shakiera Earvin <kieraearvin@gmail.com>
2025-04-01 13:24:49 -0400

    added models, orm in terminal and app

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cat
 
+admin.site.register(Cat)
 # Register your models here.
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
 
@@ -6,31 +7,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# views.py
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
-def cat_index(request):
-    # Render the cats/index.html template with the cats data
-    return render(request, 'cats/index.html', {'cats': cats})
-
-# Define the home view function
 def home(request):
-    # Send a simple HTML response
     return render(request, 'home.html')
 
 def about(request):
        return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
