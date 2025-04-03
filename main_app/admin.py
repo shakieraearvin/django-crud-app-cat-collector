e7b518577592d4e60ba33c83aa25955f0727417a
Shakiera Earvin <kieraearvin@gmail.com>
2025-04-03 15:16:59 -0400

    added one to many

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Cat
+from .models import Cat, Feeding, Toy
 
 admin.site.register(Cat)
-# Register your models here.
+admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView 
+from .models import Cat, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 
@@ -20,7 +22,10 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form
+    })
 
 class CatCreate(CreateView):
     model = Cat
@@ -28,9 +33,34 @@ class CatCreate(CreateView):
 
 class CatUpdate(UpdateView):
     model = Cat
-    # Let's disallow the renaming of a cat by excluding the name field!
     fields = ['breed', 'description', 'age']
 
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
